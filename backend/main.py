3ce08142e0003c45eee671ea25df5640bb09c00e
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-02-06 00:56:56 +0300

    Backend: Add `CORS`

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 from litestar import Litestar
+from litestar.config.cors import CORSConfig
 
 from api import api_route
 
 
+cors_config = CORSConfig(allow_origins=["*"])
+
+
 app = Litestar(
   route_handlers=[api_route],
+  cors_config=cors_config,
 )
 
