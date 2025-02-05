0e5a7878d510a935be722333d320b0552bd7b86d
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-02-06 00:16:15 +0300

    Frontend: Add init files

---
+++ frontend/src/components/TheWelcome.vue
@@ -0,0 +1,51 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Users</template>
+    <p>TOP Users</p>
+    <p>TOP Active Users</p>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Games</template>
+    <p>Bang Dream! Girl Band Party!</p>
+    <p>Colorful Stage!</p>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>---</template>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>---</template>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>---</template>
+  </WelcomeItem>
+</template>
